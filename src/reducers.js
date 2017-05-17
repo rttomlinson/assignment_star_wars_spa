8351e20566e123fe229c329c478f450ef75b6a05
Renzo Tomlinson <rttomlinson@gmail.com>
2017-05-17 18:23:17 +0000

    Create separate reducers file

---
+++ src/reducers.js
@@ -0,0 +1,38 @@
+import {MAKE_REQUEST, REQUEST_SUCCESS, REQUEST_FAILURE} from "../actions/types";
+
+
+const INITIAL_STATE = {
+  isFetching: false,
+  error: null,
+  resource: {
+      count: 0,
+      next: null,
+      previous: null,
+      results: []
+  }
+};
+
+export function swampiApp(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case MAKE_REQUEST:
+      return {
+        ...state,
+        isFetching: true,
+        error: null
+      };
+    case REQUEST_SUCCESS:
+      return {
+        ...state,
+        resource: action.data,
+        isFetching: false
+      };
+    case REQUEST_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+}
